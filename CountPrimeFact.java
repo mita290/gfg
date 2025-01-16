9c965dae6b0b977a29c256616d4563b50add4f3d
mageshmanjushree290@gmail.com <mageshmanjushree290@gmail.com>
2025-01-16 17:24:59 +0530

    more efficient solution

--- CountPrimeFact.java
+++ CountPrimeFact.java
@@ -4,27 +4,51 @@ public class CountPrimeFact {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 2; i < n; i++) {
-            if ((n % i == 0) && isPrime(i)) {
-                int x = i;
-                while (n % x == 0) {
+        if (n <= 1) {
+            System.exit(0);
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {    
+                while (n % i == 0) {
                     System.out.print(i + " ");
-                    x = x * i;
+                    n /= i;
                 }
                 System.out.println();
             }
         }
+        if (n > 1) {
+            System.out.println(n);
+        }
         sc.close();
     }
-    public static boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
+
+
+// public class CountPrimeFact {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 2; i < n; i++) {
+//             if ((n % i == 0) && isPrime(i)) {
+//                 int x = i;
+//                 while (n % x == 0) {
+//                     System.out.print(i + " ");
+//                     x = x * i;
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//         sc.close();
+//     }
+//     public static boolean isPrime(int n) {
+//         if (n < 2) {
+//             return false;
+//         }
+//         for (int i = 2; i <= Math.sqrt(n); i++) {
+//             if (n % i == 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
